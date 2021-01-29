b499f213d42a98e12053f6459c25e70545538cab
WazzBtD <27006033+WazzBtD@users@users.noreply.github.com>
2021-01-29 19:12:29 +0100

    cleanup

--- hadoop/test.py
+++
@@ -1,6 +0,0 @@
-from hdfs import InsecureClient
-import json
-client = InsecureClient('http://10.7.38.62:9870')
-client.write('test.json', json.dumps({"test-key": "test-val"}))
-
-
--- peframe/test.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-process = subprocess.Popen(['sleep', '5'],
-                     stdout=subprocess.PIPE, 
-                     stderr=subprocess.PIPE)
-stdout, stderr = process.communicate()
-print(stdout, stderr)
